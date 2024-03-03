64834ebb2fdcf6eae5e6e86dd4575dc514a2c2d2
Patrick Canady <peej@ifoundry.com>
2024-03-02 20:21:10 -0500

    Added colored notes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,8 +14,8 @@ public class Spawner : MonoBehaviour {
     public float minAngle = -15f;
     public float maxAngle = 15f;
 
-    public float minForce = 5f;
-    public float maxForce = 10f;
+    public float minForce = 10f;
+    public float maxForce = 15f;
 
     public float maxLifetime = 5f;
 
@@ -37,15 +37,31 @@ public class Spawner : MonoBehaviour {
         while (enabled) {
             GameObject prefab = notePrefabs[Random.Range(0, notePrefabs.Length)];
 
-            Vector3 position = new Vector3();
-            position.x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
-            position.y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y);
-            position.z = 0;
+            Vector3 position = new()
+            {
+                x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
+                y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y),
+                z = 0
+            };
 
             Quaternion rotation = Quaternion.Euler(0f, 0f, Random.Range(minAngle, maxAngle));
 
             GameObject note = Instantiate(prefab, position, rotation, this.transform.parent.GameObject().transform);
             Destroy(note, maxLifetime);
+
+            if (note.name.Contains("Blue")) {
+                minForce = 14;
+                maxForce = 16;
+            } else if (note.name.Contains("Red")) {
+                minForce = 12;
+                maxForce = 14;
+            } else if (note.name.Contains("Yellow")) {
+                minForce = 11;
+                maxForce = 13;
+            } else {
+                minForce = 10;
+                maxForce = 12;
+            }
 
             float force = Random.Range(minForce, maxForce);
             note.GetComponent<Rigidbody2D>().AddForce(note.transform.up * force, ForceMode2D.Impulse);
